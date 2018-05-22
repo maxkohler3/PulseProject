bf1fbc33329a2c63dbc0577ba118a81dcdddec73
Elise Carmichael <elise@Elises-MacBook-Pro.local>
2018-05-22 08:54:13 -0400

    added implementation steps for subtraction

--- src/test/java/com/glue/MathStep.java
+++ src/test/java/com/glue/MathStep.java
@@ -11,6 +11,7 @@ import java.util.List;
 public class MathStep {
 
 	int sum = 0;
+	int difference = 0;
 	
     @Given("^a list of numbers$")
     public void add_number(DataTable numberList) throws Throwable {
@@ -32,4 +33,32 @@ public class MathStep {
     public void verify_answer(int x) throws Throwable {
         Assert.assertEquals(sum, x);
     }
+    
+    
+    List<List<String>> data;
+    @Given("^I have two numbers$")
+    public void getNumbers(DataTable numberList) throws Throwable {
+    		data = numberList.raw();    		
+        Assert.assertEquals(true, true);
+    }
+
+    @When("^I subtract the second number from the first$")
+    public void substract_numbers() throws Throwable {
+    		
+    		difference = Integer.parseInt(data.get(0).get(0)) - Integer.parseInt(data.get(1).get(0));
+    	
+        Assert.assertEquals(true, true);
+    }
+
+    @Then("^I should get the number \"(.*?)\"$")
+    public void subtraction_answer(int x) throws Throwable {
+        Assert.assertEquals(difference, x);
+    }
+    
+    
+//    Given I have two numbers
+//    | 10 |
+//    | 3  |
+//  When I subtract the second number from the first
+//  Then I should get the number "7"
 }
